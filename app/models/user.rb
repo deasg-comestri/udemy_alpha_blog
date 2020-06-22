ecd548610b65308898b6dcbe3431815a059b407c
Gavin Deas <gavin.deas@comestri.com>
2020-06-22 16:53:32 +0100

    Add users table and User model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+
+  # has_many :articles
+  # validates :username, presence: true, length: { minimum: 6, maximum: 100 }
+  # validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
+end
