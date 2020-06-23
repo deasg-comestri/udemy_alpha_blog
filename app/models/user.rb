38790c862020fe1b56755d981e09b02745a9dc57
Gavin Deas <gavin.deas@comestri.com>
2020-06-23 10:33:35 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
 
   # has_many :articles
-  # validates :username, presence: true, length: { minimum: 6, maximum: 100 }
-  # validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 
 end
