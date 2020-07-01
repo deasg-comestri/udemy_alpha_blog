240445e646d08d33918b18f5e63bd3a7656d21eb
Gavin Deas <gavin.deas@comestri.com>
2020-07-01 17:53:42 +0100

    Add user authentication and logged-in session state

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user
+    else
+      flash.now[:alert] = 'Email and/or password were incorrect'
+      render 'new'
+    end
   end
 
   def destroy
-  end
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to root_path
+end
 
 end
