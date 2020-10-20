e8785b7da110fdc0d56133d94c733c986bfd3c17
Gavin Deas <gavin.deas@comestri.com>
2020-10-20 06:26:40 +0100

    Add category select to article creation form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ class ArticlesController < ApplicationController
 
   def create
     # render plain: params[:article]
+    byebug
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -55,7 +56,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
